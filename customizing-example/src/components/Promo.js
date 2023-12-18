86e4e189e003de47f7cc5e4430e364303ace5406
enrique-dominguez <enriquejosedominguez@gmail.com>
2023-12-18 14:03:38 -0400

    updated the Promo component and added a PromoHeading child component to illustrate the use of parent-child data sharing between components

--- customizing-example/src/components/Promo.js
+++ customizing-example/src/components/Promo.js
@@ -1,12 +1,20 @@
+import PromoHeading from "./PromoHeading";
+
+/* Create a single source-of-truth for all the components that will use similar information and then pass the data as props */
+const data = {
+    heading: "99% off on all products", // data is saved in an object called data
+    callToAction: "Everything must go coñaso"
+}
+
+
+
 function Promo() {
     return (
         <div className="promo-section">
-            <div>
-                <h1>Don't miss this deal!</h1>
-                </div>
-                <div>
-                <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
-            </div>
+            <PromoHeading 
+                heading={data.heading} /* the information is passed from the parent to the child */
+                callToAction={data.callToAction} 
+            />
         </div>
     );
 };
